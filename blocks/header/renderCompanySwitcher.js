8355a8e16adf6a95540cc82322e29df81337a9b1
Alex Kolesnyk <kolesnyk@adobe.com>
2025-08-07 15:41:19 -0500

    Add company switcher to header

---
+++ blocks/header/renderCompanySwitcher.js
@@ -0,0 +1,24 @@
+import { render } from '@dropins/storefront-company-management/render.js';
+import { CompanySwitcher } from '@dropins/storefront-company-management/containers/CompanySwitcher.js';
+
+/**
+ * Renders the Company Switcher directly inline in the header navigation tools
+ * @param {Element} navTools - The navigation tools container element
+ */
+export default function renderCompanySwitcher(navTools) {
+  const companySwitcherContainer = document.createRange().createContextualFragment(`
+    <div class="company-switcher-wrapper nav-tools-wrapper">
+      <div class="company-switcher-inline"></div>
+    </div>
+  `);
+
+  navTools.append(companySwitcherContainer);
+
+  const companySwitcherElement = navTools.querySelector('.company-switcher-inline');
+
+  render.render(CompanySwitcher, {})(companySwitcherElement);
+
+  return {
+    companySwitcherElement,
+  };
+}
