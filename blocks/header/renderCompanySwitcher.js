aa60adb3942deebcab47534f10c764e02b47e106
Eugene Shakhsuvarov <ishakhsuvarov@magento.com>
2025-08-11 09:06:35 -0500

    USF-2638: Basic company switcher implementation

--- blocks/header/renderCompanySwitcher.js
+++ blocks/header/renderCompanySwitcher.js
@@ -1,5 +1,5 @@
-import { render } from '@dropins/storefront-company-management/render.js';
-import { CompanySwitcher } from '@dropins/storefront-company-management/containers/CompanySwitcher.js';
+import { render } from '@dropins/storefront-company-switcher/render.js';
+import { CompanySwitcher } from '@dropins/storefront-company-switcher/containers/CompanySwitcher.js';
 
 /**
  * Renders the Company Switcher directly inline in the header navigation tools
